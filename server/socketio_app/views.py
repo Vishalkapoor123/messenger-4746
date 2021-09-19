1dae2759d3f143ba2abaae09fb0e7ba3d30d93af
vishal.kapoor <vishal.kapoor@cgi.com>
2021-09-19 12:50:30 +0530

    Resolved real-time messages for new conversations and event emitting for a single user

--- server/socketio_app/views.py
+++ server/socketio_app/views.py
@@ -15,25 +15,27 @@ thread = None
 def connect(sid, environ):
     sio.emit("my_response", {"data": "Connected", "count": 0}, room=sid)
 
-
+#add and store users with sid in a dictionary
 @sio.on("go-online")
 def go_online(sid, user_id):
-    if user_id not in online_users:
-        online_users.append(user_id)
-    sio.emit("add-online-user", user_id, skip_sid=sid)
+    online_users[user_id] = sid
+    sio.emit("add-online-user", user_id)
 
-
+#emit new message for online user else emit nothing
 @sio.on("new-message")
 def new_message(sid, message):
-    sio.emit(
-        "new-message",
-        {"message": message["message"], "sender": message["sender"]},
-        skip_sid=sid,
-    )
+    for user in online_users.keys():
+        if(user == message["recipientId"]):
+            recipient_sid = online_users[user]
+            sio.emit("new-message",
+            {"message": message["message"], "sender": message["sender"]},
+            skip_sid = sid,
+            to = recipient_sid
+            )
 
-
+#remove user from dictionary
 @sio.on("logout")
 def logout(sid, user_id):
     if user_id in online_users:
-        online_users.remove(user_id)
-    sio.emit("remove-offline-user", user_id, skip_sid=sid)
+        del online_users[user_id]
+    sio.emit("remove-offline-user", user_id)
